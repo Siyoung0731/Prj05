3a18aacd2ddac4d109aca17ebec68a5463c72886
Siyoung0731 <azxx0731@gmail.com>
2026-03-24 17:16:58 +0900

    TestCar01 생성

---
+++ src/ex01/TestCalc01.java
@@ -0,0 +1,35 @@
+package ex01;
+
+public class TestCalc01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name1 = "아빠차";
+		String color1 = "Black";
+		int year1 = 2023;
+		
+		System.out.println("차: " + name1);
+		System.out.println("색: " + color1);
+		System.out.println("연식: " + year1);
+		System.out.println();
+		
+		String name2 = "엄마차";
+		String color2 = "Red";
+		int year2 = 2025;
+		
+		System.out.println("차: " + name2);
+		System.out.println("색: " + color2);
+		System.out.println("연식: " + year2);
+		System.out.println();
+		
+		String name3 = "내차";
+		String color3 = "Green";
+		int year3 = 2021;
+		
+		System.out.println("차: " + name3);
+		System.out.println("색: " + color3);
+		System.out.println("연식: " + year3);
+		System.out.println();
+	}
+
+}
